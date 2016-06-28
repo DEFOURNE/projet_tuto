e4e086267b5b8f078fefec140808f6a9edd54415
Faustine DEFOURNE <defourne.f@gmail.com>
2016-06-28 16:38:57 +0200

    maj 28/06

---
+++ mail.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<TITLE>Donkili-Do</TITLE>
+	<link rel="stylesheet" href="form.css"/>
+	<link href="LOGO_accueil2.png" rel="shortcut icon" >
+</head>
+
+
+<body>
+<div class="header">
+	<a href="accueil.html"><div class="photo"></div></a>
+   	<center><H1> CHORALE DE PUGET-SUR-DURANCE</H1></center>
+</div>
+<?php
+
+ $mail = 'faust.def@gmail.com'; // Déclaration de l'adresse de destination.
+
+	if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+
+		{
+
+		    $passage_ligne = "\r\n";
+
+		}
+
+	else
+
+		{
+
+		    $passage_ligne = "\n";
+
+		}
+
+//=====Déclaration des messages au format texte et au format HTML.
+
+ $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
+
+//==========
+
+ 
+
+//=====Création de la boundary
+
+ $boundary = "-----=".md5(rand());
+
+//==========
+
+ 
+
+//=====Définition du sujet.
+
+ $sujet = "Hey mon ami !";
+
+//=========
+
+ 
+
+//=====Création du header de l'e-mail.
+
+ $header = "From: \"WeaponsB\"<weaponsb@mail.fr>".$passage_ligne;
+
+ $header.= "Reply-to: \"WeaponsB\" <weaponsb@mail.fr>".$passage_ligne;
+
+ $header.= "MIME-Version: 1.0".$passage_ligne;
+
+ $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+
+//==========
+
+ 
+
+//=====Création du message.
+
+ $message = $passage_ligne."--".$boundary.$passage_ligne;
+
+//=====Ajout du message au format texte.
+
+ $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+
+ $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+
+ $message.= $passage_ligne.$message_txt.$passage_ligne;
+
+//==========
+
+ $message.= $passage_ligne."--".$boundary.$passage_ligne;
+
+//=====Ajout du message au format HTML
+
+ $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+
+ $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+
+ $message.= $passage_ligne.$message_html.$passage_ligne;
+
+//==========
+
+ $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+
+ $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+
+//==========
+
+ 
+
+//=====Envoi de l'e-mail.
+
+ mail($mail,$sujet,$message,$header);
+
+//==========
+ Header("Location: https://dev.termwatch.es/~s15defour/progweb/projet_tuto/accueil.html");
+?>
+</body>
+</html>
